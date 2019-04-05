c947ec0f19f44e5208a7bbf6d8b607698e7cdaf6
kmhoute <kmhoute@gmail.com>
2019-04-05 16:21:23 -0500

    Update AllArrays.m

--- Algorithm/AllArrays.m
+++ Algorithm/AllArrays.m
@@ -5,6 +5,7 @@
 Mtotest = 63; %maximum nummber of M
 results = cell(Mtotest-1,1); 
 A = cell(Mtotest-1,1);
+A_bad = cell(Mtotest-1,1);
 for M = 2:Mtotest
     max_p = ((floor(64/M))-1);
     results{M-1} = cell(max_p,2);
@@ -48,9 +49,18 @@ for M = 2:Mtotest
     results{M-1}{k,2}((1:length(no_ofindexes)),1) = M; %M value
     results{M-1}{k,2}((1:length(no_ofindexes)),2) = p_actual; %p value
     results{M-1}{k,2}((1:length(no_ofindexes)),3) = results{M-1}{k,1}(no_ofindexes,1);
+    results{M-1}{k,2}((1:length(no_ofindexes)),4) = M*(p_actual+1);
+    
+    no_ofindexes_bad = find(results{M-1}{k,1}(:,2)>-13);
+    results{M-1}{k,3}((1:length(no_ofindexes_bad)),1) = M; %M value
+    results{M-1}{k,3}((1:length(no_ofindexes_bad)),2) = p_actual; %p value
+    results{M-1}{k,3}((1:length(no_ofindexes_bad)),3) = results{M-1}{k,1}(no_ofindexes_bad,1);
+    results{M-1}{k,3}((1:length(no_ofindexes_bad)),4) = M*(p_actual+1);
     end
-    A{M-1}=cat(1,results{M-1}{:,2});
+    A{M-1}=cat(1,results{M-1}{:,2}); % save the good
+    A_bad{M-1}=cat(1,results{M-1}{:,3}); % save the bad
 end
+%%%% good cases file %%%%
 B = cat(1,A{:}); %These give all possibilities for a successful array
 B = unique(B,'rows');
 B = sortrows(B,[-3,-1,-2]); 
@@ -63,3 +73,17 @@ for i=1:length(B)
     sub2s{i,2} = B(i,3);
 end
 save('sub2s.mat','sub2s') %Saves the matrix 
+
+%%%% bad cases file %%%%
+B_bad = cat(1,A_bad{:}); %These give all possibilities for a successful array
+B_bad = unique(B_bad,'rows');
+B_bad = sortrows(B_bad,[-3,-1,-2]); 
+save('B_bad.mat','B_bad') %Saves the matrix 
+
+sub2s_bad = cell(length(B_bad),2);
+for i=1:length(B_bad)
+    sub2s_bad{i,1} = false(1,B_bad(i,3));
+    sub2s_bad{i,1}(1:B_bad(i,1):end)=true;
+    sub2s_bad{i,2} = B_bad(i,3);
+end
+save('sub2s_bad.mat','sub2s_bad') %Saves the matrix 
