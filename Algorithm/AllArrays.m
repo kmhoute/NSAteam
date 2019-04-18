69ff61e2b43309eed1b837a756f3671483e6e155
kmhoute <kmhoute@gmail.com>
2019-04-18 18:19:08 -0500

    Update AllArrays.m

--- Algorithm/AllArrays.m
+++ Algorithm/AllArrays.m
@@ -1,89 +1,83 @@
-% AllArrays
-% This script combines the changing SLH and testingallMandp. This
-% function loops through M, calculates and loops through p, and then tests
-% all values of L. 
-Mtotest = 63; %maximum nummber of M
-results = cell(Mtotest-1,1); 
-A = cell(Mtotest-1,1);
-A_bad = cell(Mtotest-1,1);
-for M = 2:Mtotest
-    max_p = ((floor(64/M))-1);
-    results{M-1} = cell(max_p,2);
-    for k = 1:max_p+1
-        p_actual = k-1;
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%        
-        peak_compare = zeros(64, 3);
-        while (M+M*p_actual+1)>=64
-            %disp('Error1')
-            %disp(num2str(p_actual))
-            %disp(num2str(M))
-            max_p=max_p-1;
-            break
-        end
-    for L = (M+M*p_actual+1):64  % comment this line to run beampattern only; 
-                      % uncomment for prod vs min analysis on # of sensors
-        [~,~,~,Bmin, Bprod, N,~] = BP_Formation.Nested(M,p_actual,L,0);
-
-        if (p_actual+1)*M>=N*M
-            disp('Error2')
-        end
+function [finalConfigs_good, sub2s, finalConfigs_bad, sub2s_bad] = AllArrays(maxL)
+%%% This function combines the changing SLH and testingallMandp. 
+%%%      maxL is the maximum aperture you want to generate
+%%% This function loops through all M, loops through all L, and then calculates 
+%%% and loops through p. 
+%clear all; clc;
+tic;
+%maxM = 63;
+%maxL = 64; 
+    finalConfigs_good = zeros(1,4);
+    finalConfigs_bad = zeros(1,4);
     
-        Bmin_MinPos = islocalmin(Bmin(floor((length(Bmin)/2)):end));
-        locmin_1_min = find(Bmin_MinPos~=0,1);
-        locmin_1_min = round(length(Bmin)/2)+locmin_1_min-1;
-        Bprod_MinPos = islocalmin(Bprod(floor((length(Bprod)/2)):end));
-        locmin_1_prod = find(Bprod_MinPos~=0,1);
-        locmin_1_prod = round(length(Bmin)/2)+locmin_1_prod-1;
-        peaks_Bmin  = max(Bmin(locmin_1_min:end));
-        peaks_Bprod = max(Bprod(locmin_1_prod:end));
+    for M = 2:(maxL-1)
+        for L = (M+1):maxL
+            max_p = ((floor(L/M))-1);
+            if (M+M*max_p)>(L-1)
+                %disp('Error1')
+                %disp(num2str(p))
+                %disp(num2str(M))
+                %disp(num2str(L))
+                max_p=max_p-1;
+            end
+            
+            for p = 0:max_p
+                peak_compare = zeros((max_p+1), 2);
+                [~,~,~,Bmin, ~, N,~] = BP_Formation.Nested(M,p,L,0);
+
+                Bmin_MinPos = islocalmin(Bmin(floor((length(Bmin)/2)):end));
+                locmin_1_min = find(Bmin_MinPos~=0,1);
+                locmin_1_min = round(length(Bmin)/2)+locmin_1_min-1;
+                
+                peak_compare((p+1),2) = max(Bmin(locmin_1_min:end));
+                peak_compare((p+1),1) = p;
+            end % ends ext factor (p) loop  
+
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%        
+            no_ofindexes = find(peak_compare(:,2)<=-13);
+            Configs_good(1:length(no_ofindexes),1) = M;
+            Configs_good(1:length(no_ofindexes),2) = p;
+            Configs_good(1:length(no_ofindexes),3) = L;
+            Configs_good(1:length(no_ofindexes),4) = M*(p+1);
+            
+            finalConfigs_good = cat(1,finalConfigs_good(:,:), Configs_good(:,:));
+           
+            no_ofindexes_bad = find(peak_compare(:,2)>-13);
+            Configs_bad(1:length(no_ofindexes_bad),1) = M;
+            Configs_bad(1:length(no_ofindexes_bad),2) = p;
+            Configs_bad(1:length(no_ofindexes_bad),3) = L;
+            Configs_bad(1:length(no_ofindexes_bad),4) = M*(p+1);
+            
+            finalConfigs_bad = cat(1,finalConfigs_bad(:,:),Configs_bad(:,:));
+        end % ends L loop
         
-        peak_compare(L, 2) = peaks_Bmin;
-        peak_compare(L, 3) = peaks_Bprod;
-        peak_compare(L,1) = M*(N-1)+1;
-    end  %%%%% comment this line and the following lines to run beampattern only; 
-       %%%% uncomment for prod vs min analysis on # of sensors
-    
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%        
-    results{M-1}{k,1} = peak_compare;
-    no_ofindexes = find(results{M-1}{k,1}(:,2)<=-13);
-    results{M-1}{k,2}((1:length(no_ofindexes)),1) = M; %M value
-    results{M-1}{k,2}((1:length(no_ofindexes)),2) = p_actual; %p value
-    results{M-1}{k,2}((1:length(no_ofindexes)),3) = results{M-1}{k,1}(no_ofindexes,1);
-    results{M-1}{k,2}((1:length(no_ofindexes)),4) = M*(p_actual+1);
-    
-    no_ofindexes_bad = find(results{M-1}{k,1}(:,2)>-13);
-    results{M-1}{k,3}((1:length(no_ofindexes_bad)),1) = M; %M value
-    results{M-1}{k,3}((1:length(no_ofindexes_bad)),2) = p_actual; %p value
-    results{M-1}{k,3}((1:length(no_ofindexes_bad)),3) = results{M-1}{k,1}(no_ofindexes_bad,1);
-    results{M-1}{k,3}((1:length(no_ofindexes_bad)),4) = M*(p_actual+1);
+    end % ends M loop
+   %%%%% good cases file %%%%%
+    finalConfigs_good = unique(finalConfigs_good,'rows');
+    finalConfigs_good = sortrows(finalConfigs_good,[-3,-1,-2]);
+    finalConfigs_good = finalConfigs_good(1:(end-1),:);
+    save('finalConfigs_good.mat','finalConfigs_good') %Saves the matrix 
+
+    sub2s = cell(length(finalConfigs_good),2);
+    for i=1:length(finalConfigs_good)
+        sub2s{i,1} = false(1,finalConfigs_good(i,3));
+        sub2s{i,1}(1:finalConfigs_good(i,1):end)=true;
+        sub2s{i,2} = finalConfigs_good(i,3);
     end
-    A{M-1}=cat(1,results{M-1}{:,2}); % save the good
-    A_bad{M-1}=cat(1,results{M-1}{:,3}); % save the bad
-end
-%%%% good cases file %%%%
-B = cat(1,A{:}); %These give all possibilities for a successful array
-B = unique(B,'rows');
-B = sortrows(B,[-3,-1,-2]); 
-save('B.mat','B') %Saves the matrix 
+    save('sub2s.mat','sub2s') %Saves the matrix 
 
-sub2s = cell(length(B),2);
-for i=1:length(B)
-    sub2s{i,1} = false(1,B(i,3));
-    sub2s{i,1}(1:B(i,1):end)=true;
-    sub2s{i,2} = B(i,3);
-end
-save('sub2s.mat','sub2s') %Saves the matrix 
+   %%%%% bad cases file %%%%%
+    finalConfigs_bad = unique(finalConfigs_bad,'rows');
+    finalConfigs_bad = sortrows(finalConfigs_bad,[-3,-1,-2]);
+    finalConfigs_bad = finalConfigs_bad(1:(end-1),:);
+    save('finalConfigs_bad.mat','finalConfigs_bad') %Saves the matrix 
 
-%%%% bad cases file %%%%
-B_bad = cat(1,A_bad{:}); %These give all possibilities for a successful array
-B_bad = unique(B_bad,'rows');
-B_bad = sortrows(B_bad,[-3,-1,-2]); 
-save('B_bad.mat','B_bad') %Saves the matrix 
-
-sub2s_bad = cell(length(B_bad),2);
-for i=1:length(B_bad)
-    sub2s_bad{i,1} = false(1,B_bad(i,3));
-    sub2s_bad{i,1}(1:B_bad(i,1):end)=true;
-    sub2s_bad{i,2} = B_bad(i,3);
+    sub2s_bad = cell(length(finalConfigs_bad),2);
+    for i=1:length(finalConfigs_bad)
+        sub2s_bad{i,1} = false(1,finalConfigs_bad(i,3));
+        sub2s_bad{i,1}(1:finalConfigs_bad(i,1):end)=true;
+        sub2s_bad{i,2} = finalConfigs_bad(i,3);
+    end
+    save('sub2s_bad.mat','sub2s_bad') %Saves the matrix 
+    toc;
 end
-save('sub2s_bad.mat','sub2s_bad') %Saves the matrix 
