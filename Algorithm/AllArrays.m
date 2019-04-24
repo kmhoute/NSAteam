551329587579fd8a03b9252a2c23351782fe119f
kmhoute <kmhoute@gmail.com>
2019-04-24 14:47:05 -0500

    Update AllArrays.m

--- Algorithm/AllArrays.m
+++ Algorithm/AllArrays.m
@@ -7,58 +7,61 @@ function [finalConfigs_good, sub2s, finalConfigs_bad, sub2s_bad] = AllArrays(max
 tic;
 %maxM = 63;
 %maxL = 64; 
-    finalConfigs_good = zeros(1,4);
-    finalConfigs_bad = zeros(1,4);
+    finalConfigs_good = zeros(1,4); % initiates the array (will become a matrix)
+    finalConfigs_bad = zeros(1,4);  % initiates the array (will become a matrix)
     
-    for M = 2:(maxL-1)
-        for L = (M+1):maxL
-            max_p = ((floor(L/M))-1);
-            if (M+M*max_p)>(L-1)
+    for M = 2:(maxL-1) % shortest M is 2
+        for L = (M+1):maxL % shortest L is 1 more than M
+            max_p = ((floor(L/M))-1); 
+            if (M+M*max_p)>(L-1) % failsafe: makes sure sub1 is not equal to L
                 %disp('Error1')
                 %disp(num2str(p))
                 %disp(num2str(M))
                 %disp(num2str(L))
-                max_p=max_p-1;
+                max_p=max_p-1; % decrease p
             end
             
-            for p = 0:max_p
-                peak_compare = zeros((max_p+1), 2);
-                [~,~,~,Bmin, ~, N,~] = BP_Formation.Nested(M,p,L,0);
-
-                Bmin_MinPos = islocalmin(Bmin(floor((length(Bmin)/2)):end));
-                locmin_1_min = find(Bmin_MinPos~=0,1);
-                locmin_1_min = round(length(Bmin)/2)+locmin_1_min-1;
+            for p = 0:max_p % loop through extension factor (indices are p+1 to start at 1 not 0)
+                peak_compare = zeros((max_p+1), 2); % matrix for psl height check
+                [u,~,~,Bmin, ~, N,~] = BP_Formation.Nested(M,p,L,0); % gennerates BP for the case
                 
-                peak_compare((p+1),2) = max(Bmin(locmin_1_min:end));
-                peak_compare((p+1),1) = p;
+                % locates first null of the minimum processed B
+                Bmin_MinPos = islocalmin(Bmin(floor((length(Bmin)/2)):end)); % logic matrix
+                locmin_1_min = find(Bmin_MinPos~=0,1); % returns the null's index in Bmin_MinPos
+                locmin_1_min = round(length(Bmin)/2)+locmin_1_min-1; % returns the null's index in Bmin 
+                
+                peak_compare((p+1),1) = p; % saves the extension factor
+                peak_compare((p+1),2) = max(Bmin(locmin_1_min:end)); % saves the max value which is the PSL height
             end % ends ext factor (p) loop  
 
         %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%        
-            no_ofindexes = find(peak_compare(:,2)<=-13);
-            Configs_good(1:length(no_ofindexes),1) = M;
-            Configs_good(1:length(no_ofindexes),2) = p;
+            % this chunk stores configs for the different p values (with the same M and L)
+            no_ofindexes = find(peak_compare(:,2)<=-13); % saves indices of successful configs
+            Configs_good(1:length(no_ofindexes),1) = M; 
+            Configs_good(1:length(no_ofindexes),2) = peak_compare(no_ofindexes,1);
             Configs_good(1:length(no_ofindexes),3) = L;
-            Configs_good(1:length(no_ofindexes),4) = M*(p+1);
+            Configs_good(1:length(no_ofindexes),4) = M*((peak_compare(no_ofindexes,1))+1); % stores totalM length
             
+            % concatenates the cases stored above to the previously tested success cases
             finalConfigs_good = cat(1,finalConfigs_good(:,:), Configs_good(:,:));
            
             no_ofindexes_bad = find(peak_compare(:,2)>-13);
             Configs_bad(1:length(no_ofindexes_bad),1) = M;
-            Configs_bad(1:length(no_ofindexes_bad),2) = p;
+            Configs_bad(1:length(no_ofindexes_bad),2) = peak_compare(no_ofindexes_bad,1);
             Configs_bad(1:length(no_ofindexes_bad),3) = L;
-            Configs_bad(1:length(no_ofindexes_bad),4) = M*(p+1);
+            Configs_bad(1:length(no_ofindexes_bad),4) = M*((peak_compare(no_ofindexes_bad,1))+1);
             
             finalConfigs_bad = cat(1,finalConfigs_bad(:,:),Configs_bad(:,:));
         end % ends L loop
-        
     end % ends M loop
-   %%%%% good cases file %%%%%
-    finalConfigs_good = unique(finalConfigs_good,'rows');
-    finalConfigs_good = sortrows(finalConfigs_good,[-3,-1,-2]);
-    finalConfigs_good = finalConfigs_good(1:(end-1),:);
-    save('finalConfigs_good.mat','finalConfigs_good') %Saves the matrix 
+    
+   %%%%% good cases file generation %%%%%
+    finalConfigs_good = unique(finalConfigs_good,'rows'); % deletes any duplicate rows
+    finalConfigs_good = sortrows(finalConfigs_good,[-3,-1,-2]); % sorts by L, M, then p
+    finalConfigs_good = finalConfigs_good(1:(end-1),:); % deletes the zeros row from initation
+    save('finalConfigs_good.mat','finalConfigs_good') % Saves the matrix to the file
 
-    sub2s = cell(length(finalConfigs_good),2);
+    sub2s = cell(length(finalConfigs_good),2); 
     for i=1:length(finalConfigs_good)
         sub2s{i,1} = false(1,finalConfigs_good(i,3));
         sub2s{i,1}(1:finalConfigs_good(i,1):end)=true;
